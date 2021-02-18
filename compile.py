e4da931e59f8133dfd461a9b035b0db031cf9598
antoninhrlt <66388237+antoninhrlt@users.noreply.github.com>
2021-02-18 17:30:20 +0100

    Add files via upload

--- compile.py
+++ compile.py
@@ -9,7 +9,7 @@ print("|_|    \__,_| |___/ |_| |_| |_.__/  ")
 print("                                    ")
 print("> Compilation ...")
 
-if os.system("g++ src/main.cpp include/raslib/*.cpp include/raslib/gpio/*.cpp include/raslib/motor/*.cpp include/raslib/socket/*.cpp -o bin/prog.com -I include -std=c++17") == 0:
+if os.system("g++ src/*.cpp include/raslib/*.cpp include/raslib/gpio/*.cpp include/raslib/motor/*.cpp include/raslib/socket/*.cpp -o bin/prog.com -I include -std=c++17") == 0:
     print("> Compilation successful")
     print("> Running ...")
     os.system("./bin/prog.com")
